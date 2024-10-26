d3a6b7d98f4df4c5784cde0a5a1e4df6c3bed71f
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-10-25 23:48:21 -0500

    Update ListaDoblementeEnlasadaCiclica.c

--- ListaDoblementeEnlasadaCiclica.c
+++ ListaDoblementeEnlasadaCiclica.c
@@ -10,7 +10,7 @@ struct Node{
 
 struct Node* Crear_Nodo(int valor){
   struct Node* newNode = malloc(sizeof(struct Node));
-  newNode->data = vainsertAt(&heap, 0, 1);lor;
+  newNode->data = valor;
   newNode->next = (newNode->prev = newNode);
   return newNode;
 }
