e3fba192c986279a6b61209515a33079af970695
Paolo-Daniel-B-A <pbenaventea@unsa.edu.pe>
2024-10-26 22:10:42 -0500

    Funcion insertar nodo se termino con una insercion de retroceso.

--- ListaDoblementeEnlasadaCiclica.c
+++ ListaDoblementeEnlasadaCiclica.c
@@ -23,15 +23,26 @@ void Insert_Nodo(struct Node** headTemp, int valor){
     newNodo->prev = temp;
     newNodo->next = temp;
     temp->prev = newNodo;
+    return;
   }
+  newNodo->next = *headTemp;
+  newNodo->prev = temp->prev->prev;
+  temp->prev->next = newNodo; 
+  temp->prev = newNodo;
+
 }
 
 int main(){
   struct Node* head = Crear_Nodo(1);  
+  
   Insert_Nodo(&head, 2);
+  Insert_Nodo(&head, 3);
+  Insert_Nodo(&head, 4);
+
   printf("%d", head->data);
   printf("%d", head->next->data);
   printf("%d", head->next->next->data);
+  printf("%d", head->next->next->next->data);
   //Imprimir(heap);
   return 0;
 }
